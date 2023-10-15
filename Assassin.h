300cd41348a44d2819335b4237e32f5838a626f6
delfoxs <140474808+delfoxs@users.noreply.github.com>
2023-10-15 13:58:58 +1030

    Update Assassin.h

--- Assassin.h
+++ Assassin.h
@@ -1,5 +1,5 @@
-#ifndef ASSASSIN
-#define ASSASSIN
+#ifndef ASSASSIN_H
+#define ASSASSIN_H
 
 #include "Role.h"
 #include <iostream>
@@ -8,38 +8,11 @@
 
 class Assassin : public Role {
 public:
-    Assassin() : Role("刺客", 120, 60, 25, 30) {}
+    Assassin();
 
-    void skill(Role& target) override {
-        if (getMana() < getSkillCost()) {
-            std::cout << "蓝量不足，无法使用技能！" << std::endl;
-            return;
-        }
-        Role::skill(target);
-        std::cout << getName() << " 使用技能攻击 " << target.getName() << "!" << std::endl;
-        target.setHP(target.getHP() - 45);
-    }
-
-    void ultimate(Role& target) override {
-    if (getCountRound() < 3) {
-        std::cout << "大招无法现在使用，请在第3回合之后释放!" << std::endl;
-    } else if (getCountUlt() == 1) {
-        std::cout << "大招已经被使用了!" << std::endl;
-    } else {
-        Role::ultimate(target);
-        std::cout << getName() << " 使用大招 " << target.getName() << "!" << std::endl;
-
-        srand(static_cast<unsigned int>(time(nullptr)));
-        int randomNumber = rand() % 10 + 1;
-
-        if (randomNumber <= 8) {
-            std::cout << getName() << " 你的大招失误了，你掷出了 " << randomNumber << "!" << std::endl;
-        } else {
-            std::cout << getName() << " 你的大招成功了，你掷出了 " << randomNumber << "!" << std::endl;
-            target.setHP(0);
-        }
-        setCountUlt(1);
-    }
-}
+    void skill(Role& target) override;
+    void ultimate(Role& target) override;
 };
+
 #endif
+
