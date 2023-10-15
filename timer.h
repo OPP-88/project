f62af3d1c09d7a07abe7a747d4a82eee26e6c87c
ZirongWang04 <140474950+ZirongWang04@users.noreply.github.com>
2023-10-15 14:27:44 +1030

    Create timer.h

---
+++ timer.h
@@ -0,0 +1,18 @@
+// Timer.h
+#ifndef TIMER_H
+#define TIMER_H
+
+class Timer {
+public:
+    Timer();
+    void start(int durationInSeconds);
+    bool isTimeUp() const;
+
+private:
+    bool timerStarted;
+    int startTime;
+    int duration;
+};
+
+#endif
+
