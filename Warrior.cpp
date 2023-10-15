91fc4b49b3d410e5ce7ab1d2062dc40463ca8566
delfoxs <140474808+delfoxs@users.noreply.github.com>
2023-10-15 13:53:40 +1030

    Create Warrior.cpp

---
+++ Warrior.cpp
@@ -0,0 +1,27 @@
+#include "Warrior.h"
+#include <iostream>
+
+Warrior::Warrior() : Role("战士", 200, 50, 20, 25) {}
+
+void Warrior::skill(Role& target) {
+    if (getMana() < getSkillCost()) {
+        std::cout << "蓝量不足，无法使用技能！" << std::endl;
+        return;
+    }
+    Role::skill(target);
+    std::cout << getName() << " 使用技能攻击 " << target.getName() << "!" << std::endl;
+    target.setHP(target.getHP() - 40);
+}
+
+void Warrior::ultimate(Role& target) {
+    if (getCountRound() < 3) {
+        std::cout << "大招无法现在使用，请在第3回合之后释放!" << std::endl;
+    } else if (getCountUlt() == 1) {
+        std::cout << "大招已经被使用了!" << std::endl;
+    } else {
+        Role::ultimate(target);
+        std::cout << getName() << " 使用大招!" << std::endl;
+        target.setMana(0);
+        setCountUlt(1);
+    }
+}
