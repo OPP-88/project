94de4461b1a6af12fa64107fe9c0cc0f25f3b685
ZirongWang04 <140474950+ZirongWang04@users.noreply.github.com>
2023-10-15 14:55:37 +1030

    Update timer.h

--- timer.h
+++ timer.h
@@ -7,6 +7,8 @@ public:
     Timer();
     void start(int durationInSeconds);
     bool isTimeUp() const;
+    int getTimeRemaining() const; // 声明获取剩余时间的函数
+    int update() const;
 
 private:
     bool timerStarted;
