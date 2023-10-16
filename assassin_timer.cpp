aadc3829b24a40a34a2a42b834f822e017836bbd
delfoxs <140474808+delfoxs@users.noreply.github.com>
2023-10-17 02:09:47 +1030

    Update assassin_timer.cpp

--- assassin_timer.cpp
+++ assassin_timer.cpp
@@ -1,40 +1,38 @@
 #include "assassin_timer.h"
-#include "assassin.h"
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
 
-Assassin::Assassin() : Role("刺客", 120, 60, 25, 30) {
+Assassin::Assassin() : Role("Assassin", 120, 60, 25, 30) {
     ultimateTimer = Timer();
-    ultimateTimer.start(14); // 设置大招计时器的持续时间，单位为秒
+    ultimateTimer.start(14);
 }
 
 void Assassin::skill(Role& target) {
     if (getMana() < getSkillCost()) {
-        std::cout << "蓝量不足，无法使用技能！" << std::endl;
+        std::cout << "mana not enough, cannot use skill attack！" << std::endl;
         return;
     }
     Role::skill(target);
-    std::cout << getName() << " 使用技能攻击 " << target.getName() << "!" << std::endl;
     target.setHP(target.getHP() - 45);
 }
 
 void Assassin::ultimate(Role& target) {
     if (!ultimateTimer.isTimeUp()) {
-        std::cout << "大招无法现在使用，请在计时结束后释放!" << std::endl;
+        std::cout << "ultimate is not ready until：" << ultimateTimer.getTimeRemaining() << " seconds" << std::endl;
     } else if (getCountUlt() == 1) {
-        std::cout << "大招已经被使用了!" << std::endl;
+        std::cout << "ultimate have been casted already!" << std::endl;
     } else {
         Role::ultimate(target);
-        std::cout << getName() << " 使用大招 " << target.getName() << "!" << std::endl;
+        std::cout << getName() << " using ultimate to " << target.getName() << "!" << std::endl;
 
         srand(static_cast<unsigned int>(time(nullptr)));
         int randomNumber = rand() % 10 + 1;
 
         if (randomNumber <= 8) {
-            std::cout << getName() << " 你的大招失误了，你掷出了 " << randomNumber << "!" << std::endl;
+            std::cout << getName() << " your ultimate failed, you have roled: " << randomNumber << " !" << std::endl;
         } else {
-            std::cout << getName() << " 你的大招成功了，你掷出了 " << randomNumber << "!" << std::endl;
+            std::cout << getName() << " your ultimate succuss, you have roled: " << randomNumber << " !" << std::endl;
             target.setHP(0);
         }
         setCountUlt(1);
