1d460be7825e6fdaaa979638c1c50fe1647f2c9a
delfoxs <140474808+delfoxs@users.noreply.github.com>
2023-10-17 02:11:13 +1030

    Update warrior_timer.cpp

--- warrior_timer.cpp
+++ warrior_timer.cpp
@@ -1,35 +1,30 @@
 #include "warrior_timer.h"
-#include <chrono>
+#include <iostream>
 
-Warrior::Warrior() : Role("战士", 200, 50, 20, 25) {
-    lastUltimateTime = std::chrono::steady_clock::now();
+Warrior::Warrior() : Role("Warrior", 200, 50, 20, 25) {
+    ultimateTimer = Timer();
+    ultimateTimer.start(12);
 }
 
 void Warrior::skill(Role& target) {
     if (getMana() < getSkillCost()) {
-        std::cout << "蓝量不足，无法使用技能！" << std::endl;
+        std::cout << "mana not enough, cannot use skill attack! " << std::endl;
         return;
     }
     Role::skill(target);
-    std::cout << getName() << " 使用技能攻击 " << target.getName() << "!" << std::endl;
     target.setHP(target.getHP() - 40);
 }
 
 void Warrior::ultimate(Role& target) {
-    auto currentTime = std::chrono::steady_clock::now();
-    auto elapsed = std::chrono::duration_cast<std::chrono::seconds>(currentTime - lastUltimateTime);
-
-    if (getCountRound() < 3) {
-        std::cout << "大招无法现在使用, 请在第3回合之后释放!" << std::endl;
+ if (!ultimateTimer.isTimeUp()) {
+std::cout << "ultimate is not ready until： " << ultimateTimer.getTimeRemaining() << " seconds " << std::endl;
     } else if (getCountUlt() == 1) {
-        std::cout << "大招已经被使用了!" << std::endl;
-    } else if (elapsed.count() < 12) { // 12 秒冷却时间
-        std::cout << "大招还在冷却中，请等待 " << 12 - elapsed.count() << " 秒后释放!" << std::endl;
+        std::cout << "ultimate have been casted already! " << std::endl;
     } else {
         Role::ultimate(target);
-        std::cout << getName() << " 使用大招 " << target.getName() << "!" << std::endl;
+        std::cout << getName() << "using ultimate to youself and " << target.getName() << " !" << std::endl;
         target.setMana(0);
+        setMana(0);
         setCountUlt(1);
-        lastUltimateTime = currentTime;
     }
 }
