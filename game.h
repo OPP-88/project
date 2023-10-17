45062e27dd1a1339efb63e462677dda355dfc5f3
ZirongWang04 <140474950+ZirongWang04@users.noreply.github.com>
2023-10-17 14:39:20 +1030

    Update game.h

--- game.h
+++ game.h
@@ -1,31 +1,28 @@
 #ifndef GAME_H
 #define GAME_H
 
-#include "warrior_timer.h"
-#include "assassin_timer.h"
-#include "mage_timer.h"
-#include "tank_timer.h"
-#include <cstdlib>
-#include <ctime>
-#include <iostream>
-
-
+#include "warrior_timer.h"   
+#include "assassin_timer.h" 
+#include "mage_timer.h"     
+#include "tank_timer.h"     
+#include <cstdlib>           // Include the standard library for utility functions.
+#include <ctime>             // Include the standard library for time functions.
+#include <iostream>        
 
 class Game {
 public:
-    Role* player;
-    Role* computer;
+    Role* player;            // Pointer to the player's role.
+    Role* computer;          // Pointer to the computer's role.
 
-    Game();
-    ~Game();
+    Game();                  // Constructor for the Game class.
+    ~Game();                 // Destructor for the Game class.
 
-    void playRound();
+    void playRound();        // Method to play a round of the game.
 
 private:
-    void initializePlayers();
-    Timer skillCooldownTimer;
-    Timer ultimateCooldownTimer;
+    void initializePlayers(); // Method to initialize player roles.
+    Timer skillCooldownTimer;   // Timer for tracking skill cooldown.
+    Timer ultimateCooldownTimer; // Timer for tracking ultimate ability cooldown.
 };
-
 
 #endif
