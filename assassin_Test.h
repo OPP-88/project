d24ca8704ccabe8b334a20c4a21b66172dade96a
delfoxs <140474808+delfoxs@users.noreply.github.com>
2023-10-17 21:27:11 +1030

    Create assassin_Test.h

---
+++ assassin_Test.h
@@ -0,0 +1,17 @@
+#ifndef ASSASSIN_TEST_H
+#define ASSASSIN_TEST_H
+
+#include <iostream>
+#include "assassin_timer.h"
+
+class AssassinTest {
+public:
+    void runTests();
+
+private:
+    void testAssassinInitialization();
+    void testAssassinSkill();
+    void testAssassinUltimate();
+};
+
+#endif
