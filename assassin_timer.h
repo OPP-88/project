25504e0ef3b0c32596f0218667f1ebf1f9e8c1cf
ZirongWang04 <140474950+ZirongWang04@users.noreply.github.com>
2023-10-17 15:13:17 +1030

    Update assassin_timer.h

--- assassin_timer.h
+++ assassin_timer.h
@@ -1,18 +1,17 @@
 #ifndef ASSASSIN_TIMER_H
 #define ASSASSIN_TIMER_H
 
-#include "Role.h"
-#include "timer.h"
+#include "Role.h"    // Include the header for the Role class.
+#include "timer.h"   // Include the header for the timer class.
 
 class Assassin : public Role {
 public:
-    Assassin();
-
-    void skill(Role& target) override;
-    void ultimate(Role& target) override;
+    Assassin();              // Constructor for the Assassin class.
+    void skill(Role& target) override;    // Declaration of the skill method.
+    void ultimate(Role& target) override; // Declaration of the ultimate method.
 
 private:
-    Timer ultimateTimer;
+    Timer ultimateTimer;   // Timer used for the Assassin's ultimate ability.
 };
 
 #endif
