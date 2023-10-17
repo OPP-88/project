27e9153233e6482d35e489551da627e6b68accba
ZirongWang04 <140474950+ZirongWang04@users.noreply.github.com>
2023-10-17 15:23:10 +1030

    Update mage_timer.h

--- mage_timer.h
+++ mage_timer.h
@@ -1,18 +1,17 @@
-// Mage_Timer.h
 #ifndef MAGE_TIMER_H
 #define MAGE_TIMER_H
 
 #include "Role.h"
 #include "timer.h"
 
-class Mage: public Role {
+class Mage : public Role {
 public:
-    Mage();
-    void skill(Role& target) override;
-    void ultimate(Role& target) override;
+    Mage(); // Constructor for the Mage class
+    void skill(Role& target) override; // Skill function to perform an attack on the target
+    void ultimate(Role& target) override; // Ultimate function for a powerful attack
 
 private:
-    Timer ultimateTimer;
+    Timer ultimateTimer; // Timer to manage the cooldown of the ultimate ability
 };
 
 #endif
