200afede690c274ed04284d4f2cd9930749d7706
ZirongWang04 <140474950+ZirongWang04@users.noreply.github.com>
2023-10-17 15:25:26 +1030

    Update tank_timer.cpp

--- tank_timer.cpp
+++ tank_timer.cpp
@@ -1,34 +1,34 @@
-#include "tank_timer.h"
-#include "timer.h"
-#include <iostream>
-#include <cmath>
+#include "tank_timer.h" // Include the tank_timer header file.
+#include "timer.h"      // Include the timer header file.
+#include <iostream>     // Include the standard input/output stream library.
+#include <cmath>        // Include the math library.
 
 Tank::Tank() : Role("Tank", 200, 40, 20, 20) {
-    ultimateTimer = Timer();
-    ultimateTimer.start(16);
+    ultimateTimer = Timer(); // Create an instance of the Timer class for the tank's ultimate ability cooldown.
+    ultimateTimer.start(16); // Start the cooldown timer with a duration of 16 seconds.
 }
 
 void Tank::skill(Role& target) {
     if (getMana() < getSkillCost()) {
         std::cout << "mana not enough, cannot use skill attack" << std::endl;
-        return;
+        return; // Check if the tank's mana is sufficient for the skill; if not, print an error message and return.
     }
-    Role::skill(target);
-    target.setHP(target.getHP() - 25);
+    Role::skill(target); // Call the base class (Role) skill function on the target.
+    target.setHP(target.getHP() - 25); // Reduce the target's HP by 25.
 }
 
 void Tank::ultimate(Role& target) {
     if (!ultimateTimer.isTimeUp()) {
-std::cout << "ultimate is not ready until： " << ultimateTimer.getTimeRemaining() << " seconds " << std::endl;
+        std::cout << "ultimate is not ready until: " << ultimateTimer.getTimeRemaining() << " seconds " << std::endl;
     } else if (getCountUlt() == 1) {
-        std::cout << "ultimate have been casted already! " << std::endl;
+        std::cout << "ultimate has already been cast! " << std::endl;
     } else {
-        Role::ultimate(target);
-        std::cout << getName() << "using ultimate to! " << std::endl;
+        Role::ultimate(target); // Call the base class (Role) ultimate function on the target.
+        std::cout << getName() << " using ultimate! " << std::endl;
 
-        setHP(std::ceil(getHP() * 0.6));
-        setAttack(getAttack() * 3);
-        std::cout << "your ultimate succuss, you lost "<<(getHP()/0.6)-getHP()<< " but you attack increase to "<<getAttack() << std::endl;
-        setCountUlt(1);
+        setHP(std::ceil(getHP() * 0.6)); // Increase the tank's HP by 60% (rounding up).
+        setAttack(getAttack() * 3);     // Triple the tank's attack power.
+        std::cout << "Your ultimate was successful. You lost " << (getHP() / 0.6) - getHP() << " HP, but your attack increased to " << getAttack() << std::endl;
+        setCountUlt(1); // Set the ultimate count to 1, indicating that the ultimate has been cast.
     }
 }
