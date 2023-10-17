f616f2fbb32f37e19600e8206644bc69d9824fca
ZirongWang04 <140474950+ZirongWang04@users.noreply.github.com>
2023-10-17 15:11:31 +1030

    Update assassin_timer.cpp

--- assassin_timer.cpp
+++ assassin_timer.cpp
@@ -1,40 +1,49 @@
-#include "assassin_timer.h"
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
+#include "warrior_timer.h"  // Include the header for the warrior_timer class.
+#include <chrono>          // Include the header for time-related operations.
 
-Assassin::Assassin() : Role("Assassin", 180, 90, 30, 30) {
-    ultimateTimer = Timer();
-    ultimateTimer.start(14);
+Warrior::Warrior() : Role("Warrior", 200, 50, 20, 25) {
+    lastUltimateTime = std::chrono::steady_clock::now();
+    // Initialize the lastUltimateTime with the current time using the steady clock.
 }
 
-void Assassin::skill(Role& target) {
+void Warrior::skill(Role& target) {
+    // Skill method for the Warrior.
     if (getMana() < getSkillCost()) {
-        std::cout << "mana not enough, cannot use skill attack！" << std::endl;
+        // Check if there is enough mana to use the skill.
+        std::cout << "Mana not enough, not able to use the skill!" << std::endl;
         return;
     }
     Role::skill(target);
-    target.setHP(target.getHP() - 50);
+    // Call the skill method of the base class and execute it.
+    std::cout << getName() << " uses the skill to attack " << target.getName() << "!" << std::endl;
+    // Display a message indicating the skill usage.
+    target.setHP(target.getHP() - 40);
+    // Reduce the target's HP by 40.
 }
 
-void Assassin::ultimate(Role& target) {
-    if (!ultimateTimer.isTimeUp()) {
-        std::cout << "ultimate is not ready until：" << ultimateTimer.getTimeRemaining() << " seconds" << std::endl;
+void Warrior::ultimate(Role& target) {
+    // Ultimate method for the Warrior.
+    auto currentTime = std::chrono::steady_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::seconds>(currentTime - lastUltimateTime);
+
+    if (getCountRound() < 3) {
+        // Check if the required number of rounds has passed to use the ultimate.
+        std::cout << "Ultimate cannot be used now, please release it after Round 3" << std::endl;
     } else if (getCountUlt() == 1) {
-        std::cout << "ultimate have been casted already!" << std::endl;
+        // Check if the ultimate has already been used.
+        std::cout << "Ultimate has been used already!!" << std::endl;
+    } else if (elapsed.count() < 12) { // 12 seconds cooldown time
+        // Check if the ultimate is still cooling down and display the remaining time.
+        std::cout << "Ultimate is still cooling, please wait " << 12 - elapsed.count() << " seconds before you can use it!" << std::endl;
     } else {
         Role::ultimate(target);
-        std::cout << getName() << " using ultimate to " << target.getName() << "!" << std::endl;
-
-        srand(static_cast<unsigned int>(time(nullptr)));
-        int randomNumber = rand() % 10 + 1;
-
-        if (randomNumber <= 7) {
-            std::cout << getName() << " your ultimate failed, you have roled: " << randomNumber << " !" << std::endl;
-        } else {
-            std::cout << getName() << " your ultimate succuss, you have roled: " << randomNumber << " !" << std::endl;
-            target.setHP(0);
-        }
+        // Call the ultimate method of the base class and execute it.
+        std::cout << getName() << " uses the ultimate " << target.getName() << "!" << std::endl;
+        target.setMana(0);
+        // Set the target's mana to 0.
         setCountUlt(1);
+        // Mark that the ultimate has been used.
+        lastUltimateTime = currentTime;
+        // Update the lastUltimateTime to the current time.
     }
 }
