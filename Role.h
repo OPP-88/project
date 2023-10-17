42248880c8ec787dd02435572961733b7f777c3d
ZirongWang04 <140474950+ZirongWang04@users.noreply.github.com>
2023-10-17 14:43:17 +1030

    Update Role.h

--- Role.h
+++ Role.h
@@ -17,66 +17,80 @@ private:
     int countUlt;
 
 public:
+    // Getter for the name attribute.
     string getName() const {
         return name;
     }
 
+    // Getter for the hp attribute.
     int getHP() const {
         return hp;
     }
 
+    // Getter for the mana attribute.
     int getMana() const {
         return mana;
     }
 
+    // Getter for the attack attribute.
     int getAttack() const {
         return attack;
     }
 
+    // Getter for the skillCost attribute.
     int getSkillCost() const {
         return skillCost;
     }
 
+    // Setter for the hp attribute.
     void setHP(int newHP) {
         hp = newHP;
     }
 
+    // Getter for the countRound attribute.
     int getCountRound() const {
         return countRound;
     }
 
+    // Getter for the countUlt attribute.
     int getCountUlt() const {
         return countUlt;
     }
 
+    // Setter for the countUlt attribute.
     void setCountUlt(int newCountUlt) {
         countUlt = newCountUlt;
     }
 
+    // Setter for the mana attribute.
     void setMana(int newMana) {
         mana = newMana;
     }
 
+    // Setter for the attack attribute.
     void setAttack(int newAttack) {
         attack = newAttack;
     }
 
+    // Constructor for the Role class, initializing attributes.
     Role(string name, int hp, int mana, int attack, int skillCost)
         : name(name), hp(hp), mana(mana), attack(attack), skillCost(skillCost), countRound(0), countUlt(0) {}
 
+    // Virtual method for using a skill.
     virtual void skill(Role& target) {
         if (mana < skillCost) {
-            cout << "蓝量不足，无法使用技能！" << endl;
+            cout << "Not enough mana to use the skill!" << endl;
             return;
         }
         mana -= skillCost;
     }
 
+    // Virtual method for using an ultimate ability.
     virtual void ultimate(Role& target) {
         if (countRound < 3) {
-            cout << "大招无法现在使用，请在第3回合之后释放!" << endl;
+            cout << "Ultimate cannot be used now, please release it after Round 3!" << endl;
         } else if (countUlt == 1) {
-            cout << "大招已经被使用了!" << endl;
+            cout << "Ultimate has already been used!" << endl;
         } else {
             countUlt += 1;
         }
@@ -84,4 +98,3 @@ public:
 };
 
 #endif
-
