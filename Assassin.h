63b05a29b4fe1f43452335ee875e8ca902ac8ea8
ZirongWang04 <140474950+ZirongWang04@users.noreply.github.com>
2023-10-17 14:32:17 +1030

    Update Assassin.h

--- Assassin.h
+++ Assassin.h
@@ -1,18 +1,17 @@
 #ifndef ASSASSIN_H
 #define ASSASSIN_H
 
-#include "Role.h"
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
+#include "Role.h"          
+#include <iostream>        
+#include <cstdlib>         // For random number generation.
+#include <ctime>           // For seeding the random number generator.
 
 class Assassin : public Role {
 public:
-    Assassin();
+    Assassin();            // Constructor for the Assassin class.
 
-    void skill(Role& target) override;
-    void ultimate(Role& target) override;
+    void skill(Role& target) override;     // Declaration of the skill method.
+    void ultimate(Role& target) override;  // Declaration of the ultimate method.
 };
 
 #endif
-
