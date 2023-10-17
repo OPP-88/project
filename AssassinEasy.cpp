1616f16e4b305aae57e452df125a7bcf16dcc7ef
ZirongWang04 <140474950+ZirongWang04@users.noreply.github.com>
2023-10-17 14:33:57 +1030

    Update AssassinEasy.cpp

--- AssassinEasy.cpp
+++ AssassinEasy.cpp
@@ -1,14 +1,20 @@
-#include "Assassin.h"
+#include "Assassin.h"  // Include the header file for the Assassin class.
 
-Assassin::Assassin() : Role("刺客", 120, 10000, 25, 30) {}
+Assassin::Assassin() : Role("Assassin", 120, 10000, 25, 30) {
+    // Constructor for the Assassin class, setting initial values.
+}
 
 void Assassin::skill(Role& target) {
+    // Skill method for the Assassin.
     if (getMana() < getSkillCost()) {
-        std::cout << "蓝量不足，无法使用技能！" << std::endl;
+        // Check if there is enough mana to use the skill.
+        std::cout << "Not enough mana to use the skill!" << std::endl;
         return;
     }
     Role::skill(target);
-    std::cout << getName() << " 使用技能攻击 " << target.getName() << "!" << std::endl;
+    // Call the skill method of the base class and execute it.
+    std::cout << getName() << " uses the skill to attack " << target.getName() << "!" << std::endl;
+    // Display a message indicating the skill usage.
     target.setHP(target.getHP() - 45);
-}
+    // Reduce the target's HP by 45.
 }
